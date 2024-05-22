9967a2abe74b63cd7f45def8d63b56b55ee78d3b
Tonykuang1028 <jikuang1@s.sfusd.edu>
2024-05-22 10:24:03 -0700

    added script to all state node

---
+++ state.gd
@@ -0,0 +1,26 @@
+extends Node2D
+class_name State
+
+@onready var debug = owner.find_child("debug")
+@onready var player = get_parent().find_child("player")
+@onready var animation_player = owner.find_child("AnimationPlayer")
+
+
+
+
+func _ready():
+	set_physics_process(false)
+	
+func _enter():
+	set_physics_process(true)
+	
+func _exit():
+	set_physics_process(false)
+	
+func transition():
+	pass
+	
+func _physics_process(delta):
+	transition()
+	debug.text = name
+	
