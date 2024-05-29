626c8b6021cca878f299c17a0f0650043729a9c6
Tonykuang1028 <jikuang1@s.sfusd.edu>
2024-05-29 09:46:50 -0700

    boss now can take damage and buff and dies

--- stone_golem.gd
+++ stone_golem.gd
@@ -2,8 +2,21 @@ extends CharacterBody2D
 
 @onready var player = get_parent().find_child("Player")
 @onready var sprite = $Sprite2D
+@onready var progress_bar = $UI/ProgressBar
 
 var direction : Vector2
+var DEF = 0
+
+var health = 5000:
+	set(value):
+		health = value
+		progress_bar.value = value
+		if value <= 0:
+			progress_bar.visible = false
+			find_child("FiniteStateMachine").change_state("Death")
+		elif value <= progress_bar.max_value / 2 and DEF == 0:
+			DEF = 5
+			find_child("FiniteStateMachine").change_state("ArmorBuff")
 
 func _ready():
 	set_physics_process(false)
@@ -20,3 +33,6 @@ func _process(_delta):
 func _physics_process(delta):
 	velocity = direction.normalized() * 100
 	move_and_collide(velocity * delta)
+	
+func take_damage():
+	health -= 10 - DEF
