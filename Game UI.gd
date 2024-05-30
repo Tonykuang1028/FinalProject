9dd93bff10253b889c4f281ec9a51dfce264b651
EricCa1 <ercai@s.sfusd.edu>
2024-05-30 10:15:24 -0700

    attempt to make boss spawn at certain score

--- Game UI.gd
+++ Game UI.gd
@@ -1,7 +1,10 @@
 extends Control
 
+var spawn_score_threshold = 100
+var enemy_scene = preload("res://bossroom.tscn")
 var score = 0
 @onready var scoreLabel = $Score
+
 
 func _process(delta):
 	score += .15
--- survivors_game.gd
+++ survivors_game.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
+var spawn_score_threshold = 100
+var enemy_scene = preload("res://stone_golem.tscn")
+var player_score = 0
+@onready var player = $Player
 
+func spawn_enemy():
+	var enemy_instance = enemy_scene.instance()
+	enemy_instance.position = Vector2(0,0)
+	
+func _on_score_updated(new_score):
+	player_score = new_score
+	if player_score >= spawn_score_threshold:
+		spawn_enemy()
 
 
 func spawn_mob():
